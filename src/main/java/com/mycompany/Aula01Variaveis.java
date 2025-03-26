f39a83310043ad6bbbe905c54638014bb6e919fe
ADRIANA-DANIELSKI <adriana.danielski@escola.pr.gov.br>
2025-03-26 13:50:56 +0000

    AULA 01 DE VARIAVeIS

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+    public static void main(String[] args) {
+        //Variavel longica
+
+        //Tipo primitivo
+        boolean valorLogicoPrimitivo= True;
+
+        //Tipo abstrato
+        Boolean ValorLogicoPrimitivo= False;
+    }
+}
