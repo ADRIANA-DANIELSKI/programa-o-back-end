6c3a13fce5f4e79b88fcea12943c2caa34251dbd
ADRIANA-DANIELSKI <adriana.danielski@escola.pr.gov.br>
2025-03-28 13:45:21 +0000

    aula 2 sobre variaveis

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -5,9 +5,36 @@ public class Aula01Variaveis {
         //Variavel longica
 
         //Tipo primitivo
-        boolean valorLogicoPrimitivo= True;
+        boolean valorLogicoPrimitivo= true;
+        //exibe valor
+        System.out.println(valorLogicoPrimitivo);
 
         //Tipo abstrato
-        Boolean ValorLogicoPrimitivo= False;
+        Boolean valorLogicoAbstrato = false;
+        System.out.println("o valor da variavel ValorLogicoAbstrato é: " + valorLogicoAbstrato);
+        // Variáveis inteiras
+
+        // Tipo primitivo 
+
+        int valorInteiro = 10; // 32 bits
+        long valorInteiroLongo = 10L; // 64 bits
+
+        // Tipo abstrato
+        Integer valorInteiroAbs = 20;
+        Long valorLongoAbs = 20l;
+
+        System.out.println("O valor de valorInteiro é: "+ valorInteiro +", e o valor de valorInteiroLongo é:" + valorInteiroLongo);
+
+        //Variaveis decimais
+
+        //Tipo primitivo
+        float numeroFloat = 10.5f; // 32bits
+        double numeroDouble = 10.5; // 64 bits
+
+        //Tipo abstrato
+        Float numeroFloatAbs = 10.5f;
+        Double numeroDoubleAbs = 10.5;
+
+        System.out.println("O valor de é  numeroFloat : "+ numeroFloat +", e o valor de numeroDouble é:" + numeroDouble);
     }
 }
