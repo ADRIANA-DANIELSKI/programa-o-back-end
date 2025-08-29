2fd6e24daa94ce91dbb547144bb76e5112ce6a1b
ADRIANA-DANIELSKI <adriana.danielski@escola.pr.gov.br>
2025-08-29 14:35:11 +0000

    aula 12 construtores

---
+++ src/main/java/com/mycompany/poo/aula12/App.java
@@ -0,0 +1,8 @@
+package com.mycompany.poo.aula12;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa pessoal = new Pessoa();
+        Pessoa pessoa2 = new Pessoa( "Adriana");
+    }
+}
