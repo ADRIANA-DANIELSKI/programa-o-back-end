a59914bf620fd382915220b8ec689d70570569e6
ADRIANA-DANIELSKI <adriana.danielski@escola.pr.gov.br>
2025-11-05 13:52:45 +0000

    aula 13 herança e abstração parte 3

--- src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
+++ src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
@@ -6,4 +6,14 @@ public class Golfinho extends Animal {
     public void emitirSom() {
         System.out.println("O golfinho está grunhindo...");
     }
+
+    @Override
+    public void mover() {
+        System.out.println(this.getNome() + " está nadando. . .");
+    }
+
+    @Override
+   public void mover(String direcao) {
+    System.out.println(this.getNome() + " está nadando para o(a) " + direcao + "...");
+}
 }
